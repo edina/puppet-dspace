e310ecfe87963468083bf2927ef5d818ff1644cd
George Hamilton <george.hamilton@ed.ac.uk>
2015-06-16 16:04:50 +0100

    IssueID #12016 check for existance of dspace directory

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,15 +1,14 @@
 class dspace::config {
-  $ds_root = '/usr/local/dspace'
-  $conf_dir = "${ds_root}/config"
-
-  notify{"The value is: $hostname": }
+  $conf_dir = "${dsroot}/config"
   $db_pass = hiera('db::pass')
-
   $ds_hostname = "$hostname"
   $ds_baseurl = hiera('ds::baseurl')
   $ds_url = $ds_baseurl
   $ds_name = hiera('ds::name')
 
+  file { "${dsroot}":
+    ensure => directory,
+  }
   file { "$conf_dir":
     ensure => directory,
   }
