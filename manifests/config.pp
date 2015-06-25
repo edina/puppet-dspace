62669c33b7cda216fd237be4338e54191e85710b
George Hamilton <gmh04@netscape.net>
2015-06-25 17:38:45 +0100

    add discovery, oai and solr configuration

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,19 +1,51 @@
 class dspace::config {
-  $conf_dir = "${dspace::dsroot}/config"
+
+  notice($dspace::dsroot)
+  notice($fqdn)
+
+  $db_url = hiera('db::url')
+  $db_user = hiera('db::user')
   $db_pass = hiera('db::pass')
-  $ds_hostname = "$hostname"
+
+  $ds_conf_dir = "${dspace::dsroot}/config"
+  $ds_root = $dspace::dsroot
+  $ds_hostname = $fqdn
   $ds_baseurl = hiera('ds::baseurl')
-  $ds_url = $ds_baseurl
+  $ds_url = hiera('ds::url')
   $ds_name = hiera('ds::name')
+  $ds_handleurl = hiera('ds::handleurl')
+
+  $doi_user = hiera('doi::user')
+  $doi_pass = hiera('doi::pass')
+  $doi_prefix = hiera('doi::prefix')
+  $doi_ns = hiera('doi::ns')
+
+  $google_analyticskey= hiera('google::analyticskey')
 
   file { "${dspace::dsroot}":
     ensure => directory,
   }->
-  file { "$conf_dir":
+  file { "$ds_conf_dir":
     ensure => directory,
   }->
-  file { "$conf_dir/dspace.cfg":
+  file { "$ds_conf_dir/dspace.cfg":
     ensure  => present,
     content => template('dspace/dspace.cfg.erb'),
+  }
+
+  $ds_modules_conf_dir = "$ds_conf_dir/modules"
+  $tomcat_port = hiera('tomcat::port')
+  $oai_sorage = hiera('oai::storage')
+  file { "$ds_modules_conf_dir/oai.cfg":
+    ensure  => present,
+    content => template('dspace/oai.cfg.erb'),
+  }
+  file { "$ds_modules_conf_dir/discovery.cfg":
+    ensure  => present,
+    content => template('dspace/discovery.cfg.erb'),
+  }
+  file { "$ds_modules_conf_dir/solr-statistics.cfg":
+    ensure  => present,
+    content => template('dspace/solr-statistics.cfg.erb'),
   }
 }
--- manifests/preinstall.pp
+++ manifests/preinstall.pp
@@ -1,4 +1,5 @@
 $ds_src = hiera('ds::src')
+$ds_runtime = hiera('ds::runtime')
 $db_user = hiera('db::user')
 $db_pass = hiera('db::pass')
 $db_url = hiera('db::url')
