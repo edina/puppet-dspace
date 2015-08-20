85b1956dc4dd7b321aab5c76213eff8ca33c200b
George Hamilton <gmh04@netscape.net>
2015-08-20 10:54:46 +0100

    add curate.cfg

--- manifests/preinstall.pp
+++ manifests/preinstall.pp
@@ -1,8 +1,11 @@
-$ds_src = hiera('ds::src')
+$ds_src     = hiera('ds::src')
 $ds_runtime = hiera('ds::runtime')
-$db_user = hiera('db::user')
-$db_pass = hiera('db::pass')
-$db_url = hiera('db::url')
+$db_user    = hiera('db::user')
+$db_pass    = hiera('db::pass')
+$db_host    = hiera('db::host')
+$db_name    = hiera('db::name')
+$db_port    = hiera('db::port')
+$db_url     = "jdbc:postgresql://${db_host}:${db_port}/${db_name}"
 
 file { "$ds_src/dev.properties":
   ensure  => present,
