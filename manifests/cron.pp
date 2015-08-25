e80af27eff452bb621f639e78c0ca53309a48454
George Hamilton <gmh04@netscape.net>
2015-08-25 17:04:34 +0100

    move generate-sitemaps to prime

--- manifests/cron.pp
+++ manifests/cron.pp
@@ -1,13 +1,12 @@
 class dspace::cron {
-  $ds_root    = $dspace::dsroot
-  $ds_bin_dir = "${ds_root}/bin"
-
-  cron { generate-sitemap:
-    command => "${ds_bin_dir}/dspace generate-sitemaps",
-    minute  => [0, 8, 16]
-  }
 }
 
 class dspace::cron::prime {
+  $ds_bin_dir = "${$dspace::dsroot}/bin"
 
+  cron { generate-sitemap:
+    command => "${ds_bin_dir}/dspace generate-sitemaps",
+    hour  => [0, 8, 16],
+    minute => 0
+  }
 }
