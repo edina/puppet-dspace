b2db1e030a485014f43c74ef01c3f1333ccef3d6
George Hamilton <gmh04@netscape.net>
2017-07-27 12:19:39 +0100

    add some puppet preinstall default values

--- manifests/preinstall.pp
+++ manifests/preinstall.pp
@@ -1,10 +1,10 @@
-$ds_src     = hiera('ds::src')
-$ds_runtime = hiera('ds::runtime')
-$db_user    = hiera('db::user')
-$db_pass    = hiera('db::pass')
-$db_host    = hiera('db::host')
-$db_name    = hiera('db::name')
-$db_port    = hiera('db::port')
+$ds_src     = hiera('ds::src', '/root/DSpace')
+$ds_runtime = hiera('ds::runtime', '/usr/local/datashare')
+$db_user    = hiera('db::user', 'datashar')
+$db_pass    = hiera('db::pass', 'password')
+$db_host    = hiera('db::host', 'localhost')
+$db_name    = hiera('db::name', 'datashar')
+$db_port    = hiera('db::port', '5432')
 $db_url     = "jdbc:postgresql://${db_host}:${db_port}/${db_name}"
 
 file { "$ds_src/dev.properties":
