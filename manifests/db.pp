3de20c06158b1907dcabf1ab4fe1195976b4f3db
George Hamilton <gmh04@netscape.net>
2017-08-03 14:41:24 +0100

    use correct postgres service name

--- manifests/db.pp
+++ manifests/db.pp
@@ -9,7 +9,7 @@ class dspace::db {
     manage_pg_ident_conf => false,
     }->
     class {"postgresql::server":
-      service_name => 'postgresql'
+      service_name => 'postgresql-9.3'
     }
 
   postgresql::server::role { "$db_name":
